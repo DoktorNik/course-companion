10c310eb3b8683e968f3f0ce834fe2e6d5816a8a
Doktor Nik <ziff5000@outlook.com>
2024-07-23 05:47:13 -0400

    css improvements

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -4,7 +4,7 @@
             {{ __('Students \ Show') }}
         </h2>
     </x-slot>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+    <div class="max-w-3xl mx-auto p-4 sm:p-6 lg:p-8">
         @if(is_null($student))
             <div class="bg-red-200 text-red-700 p-2.5 m-2">
                 <strong>Unable to find student!</strong>
