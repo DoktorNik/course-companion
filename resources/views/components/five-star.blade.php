bd71645075af709f3ac34872640d3e0550a43ba1
Doktor Nik <ziff5000@outlook.com>
2024-07-22 20:56:40 -0400

    css improvements

--- resources/views/components/five-star.blade.php
+++ resources/views/components/five-star.blade.php
@@ -44,13 +44,13 @@
 {{--display rating div--}}
 <div class = "flex flex-row-reverse justify-end">
     <input class="hidden peer peer-checked:opacity-100" type="radio" id="{{$id}}5" name="rating{{ucfirst($id)}}" value="5">
-    <label class="text-3xl cursor-pointer opacity-40 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}5" title="{{$tooltip}} 5/5">{!!$code!!}</label>
+    <label class="text-3xl cursor-pointer opacity-25 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}5" title="{{$tooltip}} 5/5">{!!$code!!}</label>
     <input class="hidden peer peer-checked:opacity-100" type="radio" id="{{$id}}4" name="rating{{ucfirst($id)}}" value="4">
-    <label class="text-3xl cursor-pointer opacity-40 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}4" title="{{$tooltip}} 4/5">{!!$code!!}</label>
+    <label class="text-3xl cursor-pointer opacity-25 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}4" title="{{$tooltip}} 4/5">{!!$code!!}</label>
     <input class="hidden peer peer-checked:opacity-100" type="radio" id="{{$id}}3" name="rating{{ucfirst($id)}}" value="3">
-    <label class="text-3xl cursor-pointer opacity-40 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}3" title="{{$tooltip}} 3/5">{!!$code!!}</label>
+    <label class="text-3xl cursor-pointer opacity-25 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}3" title="{{$tooltip}} 3/5">{!!$code!!}</label>
     <input class="hidden peer peer-checked:opacity-100" type="radio" id="{{$id}}2" name="rating{{ucfirst($id)}}" value="2">
-    <label class="text-3xl cursor-pointer opacity-40 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}2" title="{{$tooltip}} 2/5">{!!$code!!}</label>
+    <label class="text-3xl cursor-pointer opacity-25 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}2" title="{{$tooltip}} 2/5">{!!$code!!}</label>
     <input class="hidden peer peer-checked:opacity-100" type="radio" id="{{$id}}1" name="rating{{ucfirst($id)}}" value="1">
-    <label class="text-3xl cursor-pointer opacity-40 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}1" title="{{$tooltip}} 1/5">{!!$code!!}</label>
+    <label class="text-3xl cursor-pointer opacity-25 hover:opacity-100 peer peer-hover:opacity-100 peer-checked:opacity-100" for="{{$id}}1" title="{{$tooltip}} 1/5">{!!$code!!}</label>
 </div>
