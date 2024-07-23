ce787ddef1e24c22ac93273577837deed0ceba6c
Doktor Nik <ziff5000@outlook.com>
2024-07-22 20:34:18 -0400

    css improvements

--- resources/views/courseFeedback/show.blade.php
+++ resources/views/courseFeedback/show.blade.php
@@ -65,81 +65,85 @@ if(is_null($course))
     </x-slot>
     {{--    main div--}}
     <div class="py-6">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg my-4 mx-auto">
                 <div class="p-6 text-gray-900">
-                    <p class="font-bold text-xl">{{ $course->code }}: {{$course->name}}</p>
-                    <div class="flex justify-start w-full"></div>
-                        <div class="cursor-default ml-3 w-full" title="Difficulty">&#128547 {{$course->ratingDifficulty}}</div>
-                        <div class="cursor-default" title="Workload">&#128338 {{$course->ratingWorkload}}</div>
-                        <div class="cursor-default" title="Clarity">&#128302 {{$course->ratingClarity}}</div>
-                        <div class="cursor-default" title="Relevance">&#128175 {{$course->ratingRelevance}}</div>
-                        <div class="cursor-default" title="Interest">&#128373 {{$course->ratingInterest}}</div>
-                        <div class="cursor-default" title="Helpfulness">&#129309 {{$course->ratingHelpfulness}}</div>
-                        <div class="cursor-default" title="Experiential">&#127970 {{$course->ratingExperiential}}</div>
-                        <div class="cursor-default" title="Affect">&#128151 {{$course->ratingAffect}}</div>
+                    <p class="font-bold text-xl ml-5">{{ $course->code }}: {{$course->name}}</p>
+                    <div class="flex justify-start w-full mb-12">
+                        <div class="cursor-default ml-8" title="Difficulty">&#128547&nbsp;{{$course->ratingDifficulty}}</div>
+                        <div class="cursor-default" title="Workload">&#128338&nbsp;{{$course->ratingWorkload}}</div>
+                        <div class="cursor-default" title="Clarity">&#128302&nbsp;{{$course->ratingClarity}}</div>
+                        <div class="cursor-default" title="Relevance">&#128175&nbsp;{{$course->ratingRelevance}}</div>
+                        <div class="cursor-default" title="Interest">&#128373&nbsp;{{$course->ratingInterest}}</div>
+                        <div class="cursor-default" title="Helpfulness">&#129309&nbsp;{{$course->ratingHelpfulness}}</div>
+                        <div class="cursor-default" title="Experiential">&#127970&nbsp;{{$course->ratingExperiential}}</div>
+                        <div class="cursor-default" title="Affect">&#128151&nbsp;{{$course->ratingAffect}}</div>
+                        <div class="grow"></div>
                     </div>
 {{--                no feedback yet--}}
-                    @if(!isset($courseFeedback))
-                        <p class="font-bold text-red-500">There is no feedback available for this course yet!</p>
-                        <p class="text-sm ml-3">Please be the first to add feedback</p>
-
-                        {{--                otherwise show the feedback--}}
-                    @else
-                        @if(isset($courseFeedbackCollection))
-                            <p class="text-xl font-bold">Feedback Entry #{{$courseFeedbackCollection->currentPage()}}</p>
+                    <div class="border-2 border-gray-300 rounded-lg p-2 w-10/12 m-auto">
+                        @if(!isset($courseFeedback))
+                            <p class="font-bold text-red-500">There is no feedback available for this course yet!</p>
+                            <p class="text-sm ml-3">Please be the first to add feedback</p>
+                            {{--                otherwise show the feedback--}}
                         @else
-                        <p class="text-xl font-bold">New Feedback Entry</p>
-                        @endif
-                        <p class="text-sm">{{$courseFeedback->created_at}}</p>
-                        <div class = "">
-                                <p class="font-bold text-lg mt-12">Lecturer: <i>{{ $courseFeedback->lecturer }}</i></p>
-                            <div class="mt-2 flex justify-between w-full">
-                                <div class="w-full">
-                                    <p class="font-bold">Difficulty</p>
-                                    <x-five-star id="difficulty"></x-five-star>
+                            <div class = "flex justify-between items-center w-4/5">
+                                @if(isset($courseFeedbackCollection))
+                                    <p class="text-xl font-bold">Feedback Entry #{{$courseFeedbackCollection->currentPage()}} <br/><span class="text-sm font-normal italic ml-3">{{$courseFeedback->created_at}}</span></p>
+                                @else
+                                <p class="text-xl font-bold">New Feedback Entry</p>
+                                @endif
+                                <p class="font-bold text-lg mt-6 mb-4">Lecturer: <i>{{ $courseFeedback->lecturer }}</i></p>
+                            </div>
+                            <div class = "">
+                                <div class="mt-2 flex justify-between w-full">
+                                    <div class="w-full">
+                                        <p class="font-bold">Difficulty</p>
+                                        <x-five-star id="difficulty"></x-five-star>
+                                    </div>
+                                    <div class="pl-1 w-full">
+                                        <p class="font-bold">Workload</p>
+                                        <x-five-star id="workload"></x-five-star>
+                                    </div>
                                 </div>
-                                <div class="pl-1 w-full">
-                                    <p class="font-bold">Workload</p>
-                                    <x-five-star id="workload"></x-five-star>
+                                <div class="mt-2 flex justify-between w-full">
+                                    <div class="w-full">
+                                        <p class="font-bold">Clarity</p>
+                                        <x-five-star id="clarity"></x-five-star>
+                                    </div>
+                                    <div class="pl-1 w-full">
+                                        <p class="font-bold">Relevance</p>
+                                        <x-five-star id="relevance"></x-five-star>
+                                    </div>
+                                </div>
+                                <div class="mt-2 flex justify-between w-full">
+                                    <div class="w-full">
+                                        <p class="font-bold">Interest</p>
+                                        <x-five-star id="interest"></x-five-star>
+                                    </div>
+                                    <div class="pl-1 w-full">
+                                        <p class="font-bold">Helpfulness</p>
+                                        <x-five-star id="helpfulness"></x-five-star>
+                                    </div>
+                                </div>
+                                <div class="mt-2 flex justify-between w-full">
+                                    <div class="w-full">
+                                        <p class="font-bold">Experiential</p>
+                                        <x-five-star id="experiential"></x-five-star>
+                                    </div>
+                                    <div class="pl-1 w-full">
+                                        <p class="font-bold">Affect</p>
+                                        <x-five-star id="affect"></x-five-star>
+                                    </div>
+                                </div>
+                                <div class = "mt-6">
+                                    <p class="text-lg font-bold">Comments</p>
+                                    {!!$courseFeedback->comment!!}
                                 </div>
                             </div>
-                            <div class="mt-2 flex justify-between w-full">
-                                <div class="w-full">
-                                    <p class="font-bold">Clarity</p>
-                                    <x-five-star id="clarity"></x-five-star>
-                                </div>
-                                <div class="pl-1 w-full">
-                                    <p class="font-bold">Relevance</p>
-                                    <x-five-star id="relevance"></x-five-star>
-                                </div>
-                            </div>
-                            <div class="mt-2 flex justify-between w-full">
-                                <div class="w-full">
-                                    <p class="font-bold">Interest</p>
-                                    <x-five-star id="interest"></x-five-star>
-                                </div>
-                                <div class="pl-1 w-full">
-                                    <p class="font-bold">Helpfulness</p>
-                                    <x-five-star id="helpfulness"></x-five-star>
-                                </div>
-                            </div>
-                            <div class="mt-2 flex justify-between w-full">
-                                <div class="w-full">
-                                    <p class="font-bold">Experiential</p>
-                                    <x-five-star id="experiential"></x-five-star>
-                                </div>
-                                <div class="pl-1 w-full">
-                                    <p class="font-bold">Affect</p>
-                                    <x-five-star id="affect"></x-five-star>
-                                </div>
-                            </div>
-                            <div class = "mt-2">
-                                <p class="font-bold">Comments</p>
-                                {!!$courseFeedback->comment!!}
-                            </div>
-                        </div>
-                @endif
+                    </div>
+                    @endif
+                </div>
             </div>
 {{--        pagination--}}
             @if(isset($courseFeedbackCollection))
