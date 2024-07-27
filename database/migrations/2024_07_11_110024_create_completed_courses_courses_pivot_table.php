f15954fcb9963d4a686832dde5d407687d69eb3c
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-07-27 18:43:13 -0400

    fix table name in down migration

--- database/migrations/2024_07_11_110024_create_completed_courses_courses_pivot_table.php
+++ database/migrations/2024_07_11_110024_create_completed_courses_courses_pivot_table.php
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('completed_courses_student');
+        Schema::dropIfExists('completed_courses_courses');
     }
 };
