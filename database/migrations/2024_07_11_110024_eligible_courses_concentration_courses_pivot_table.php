49537f0a505b0451c1afcfbdc7f7ea569b8fd10f
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-07-27 18:37:48 -0400

    replace EligibleCoursesConcentration class ref with explicit string in migration file

--- database/migrations/2024_07_11_110024_eligible_courses_concentration_courses_pivot_table.php
+++ database/migrations/2024_07_11_110024_eligible_courses_concentration_courses_pivot_table.php
@@ -1,16 +1,11 @@
 <?php
 
-use App\Models\CompletedCourses;
 use App\Models\Course;
-use App\Models\EligibleCoursesConcentration;
-use App\Models\EligibleCoursesMajor;
-use App\Models\Student;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -19,7 +14,8 @@ return new class extends Migration
         Schema::create('eligible_courses_concentration_courses', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Course::class);
-            $table->foreignIdFor(EligibleCoursesConcentration::class);
+            $table->foreignId('eligible_courses_concentration_id')
+                ->constrained('eligible_courses_concentrations');
             $table->timestamps();
         });
     }
