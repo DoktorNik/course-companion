d269727077de955f37329812f5a0d4e4e2aca961
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2024-07-29 21:56:20 -0400

    use raw table name for referencing completed_courses table in migration

--- database/migrations/2024_07_11_110024_create_completed_courses_courses_pivot_table.php
+++ database/migrations/2024_07_11_110024_create_completed_courses_courses_pivot_table.php
@@ -1,14 +1,11 @@
 <?php
 
-use App\Models\CompletedCourses;
 use App\Models\Course;
-use App\Models\Student;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,7 +14,8 @@ return new class extends Migration
         Schema::create('completed_courses_courses', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Course::class);
-            $table->foreignIdFor(CompletedCourses::class);
+            $table->foreignId('completed_courses_id')
+                ->constrained('completed_courses');
             $table->timestamps();
         });
     }
